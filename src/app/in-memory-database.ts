dd4a0281a2e2e83d508eafb5bcff7509c34b8f00
Adriano Gonçalves <adriangonc@hotmail.com>
2019-08-23 19:51:26 -0300

    Melhorias no layout da página de categorias e configuração do web-api para configuração do banco em memória para testes.

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,18 @@
+import {InMemoryDbService} from "angular-in-memory-web-api"
+
+import { Category } from"./pages/categories/shared/category.model";
+
+
+export class InMemoryDataBase implements InMemoryDbService {
+    createDb(){
+        const categories: Category[] [
+            {id: 1, name: "Despesas fixas", description: "Água, Luz, Internet e telefone"},
+            {id: 2, name: "Faturas e boletos", description: "Faturas de cartão de crédito e boletos"},
+            {id: 3, name: "Salário", description: "Salário recebido"},
+            {id: 4, name: "Outras despesas", description: "Despesas não planejadas"},
+            { id: 5, name: "Lazer", description: "Passeios, jogos, cinema entre outros"},
+        ];
+        return { categories }
+    }
+    
+}
